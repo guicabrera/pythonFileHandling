48718b0ac5911770e33437824ae949e2f08c8dd7
G Cabrera <guilherme.cabrera@edu.ung.br>
2021-08-30 21:47:31 -0300

    Json File Write

--- jsonHandling/jsonCreation.py
+++ jsonHandling/jsonCreation.py
@@ -1,3 +1,17 @@
 #json libary
 import json
 
+#json dump --> to convert a python objecto to a Json File
+
+x = {
+  "name": "Carmikael",
+  "age": 10,
+  "married": False,
+  "divorced": False,
+  "children": None,
+  "pets": None,
+  "cars": None
+}
+with open("jsonHandling/myJson.json","w") as file:
+    x = json.dumps(x)
+    file.write(x)
